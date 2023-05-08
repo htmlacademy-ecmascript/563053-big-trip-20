568bbc1328a249217799a97d2363b0d2b032aa52
LINavy <hatake18@bk.ru>
2023-05-08 17:08:15 +0300

    hw module3-task отрисовывает компоненты страницы fix#1

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import BoardPresenter from './presenter/board-presenter';
 const tripMain = document.querySelector('.trip-main');
 const filterForm = document.querySelector('.trip-controls__filters');
 const tripEventsContainer = document.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter({container: tripEventsContainer})
+const boardPresenter = new BoardPresenter({container: tripEventsContainer});
 
 render(new TitleView(), tripMain, RenderPosition.AFTERBEGIN);
 render(new FilterView(), filterForm);

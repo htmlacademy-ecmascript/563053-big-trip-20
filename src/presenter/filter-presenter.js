4c0cf2f4a11a229c44c5008bba90b2036a5e949e
Che5ter <hatake18@bk.ru>
2023-05-31 19:31:22 +0300

    hw module4-task2

---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,23 @@
+import FilterView from '../view/filter-view.js';
+
+import {generateFilters} from '../mock/filter.js';
+import {render} from '../framework/render.js';
+
+export default class FilterPresenter {
+  #container = null;
+
+  #pointsModel = null;
+
+  #filters = [];
+
+  constructor({container, pointsModel}) {
+    this.#container = container;
+    this.#pointsModel = pointsModel;
+
+    this.#filters = generateFilters(this.#pointsModel.points);
+  }
+
+  init() {
+    render(new FilterView(this.#filters), this.#container);
+  }
+}
